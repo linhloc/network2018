c8c9cefdc642907355942242f04c6edcc077b60e
Admin <admin@Kagomes-MacBook-Pro-Ad.local>
2018-03-22 18:00:00 +0700

    Adding server setup code

---
+++ 03.practical.work.server.setup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+
+int main(){
+    int sockfd, clen, clientfd;
+    struct sockaddr_in saddr, caddr;
+    unsigned short port = 80;
+    if((sockfd=socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Error creating socket\n");
+    }
+    
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    saddr.sin_port = htons(port);
+    
+    if (bind(sockfd, (struct sockaddr *) &saddr, sizeof(saddr)) < 0) {
+        printf("Error binding\n");
+    }
+    if (listen(sockfd, 5) < 0) {
+        printf("Error listening\n");
+    }
+    clen=sizeof(caddr);
+    if ((clientfd=accept(sockfd, (struct sockaddr*) &caddr, (socklen_t *)&clen)) < 0) {
+        printf("Error accepting connection\n");
+    }
+	return 0;
+}
